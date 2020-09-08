62cd386221499e2dfe3c1ff506171416bc6d3ae6
jjimenez4 <60484696+jjimenez4@users.noreply.github.com>
2020-09-08 16:01:36 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test_1_Technical_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
